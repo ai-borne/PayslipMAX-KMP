ce4f72dbee533d45feb4282628a137e1a1faa388
sunilpawar <mail.sunilpawar@gmail.com>
2026-06-09 16:14:36 +0530

    Fix Indian comma formatting and improve coordinate-aware parsing

--- composeApp/src/commonMain/kotlin/com/ssbmax/pdfparser/ui/screens/PayslipReplicaScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ssbmax/pdfparser/ui/screens/PayslipReplicaScreen.kt
@@ -242,9 +242,24 @@ private fun GlossaryDialog(
 }
 
 private fun formatVal(value: Double): String {
-    return value.toLong().toString().replace(Regex("(\\d)(?=(\\d{2})+(?!\\d))")) { match ->
-        match.groupValues[1] + ","
+    val longVal = value.toLong()
+    val str = longVal.toString()
+    if (str.length <= 3) return str
+    val lastThree = str.substring(str.length - 3)
+    val remaining = str.substring(0, str.length - 3)
+    val builder = StringBuilder()
+    var i = remaining.length
+    while (i > 0) {
+        if (i >= 2) {
+            builder.insert(0, remaining.substring(i - 2, i))
+            if (i - 2 > 0) builder.insert(0, ",")
+            i -= 2
+        } else {
+            builder.insert(0, remaining.substring(0, 1))
+            i -= 1
+        }
     }
+    return "${builder.toString()},$lastThree"
 }
 
 private fun GetCreditsList(payslip: ParsedPayslip): List<Triple<String, Double, String>> {
