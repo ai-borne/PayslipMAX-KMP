9fb3baf76ef5ed043d40afb00d0bb627a1bcc843
sunilpawar <mail.sunilpawar@gmail.com>
2026-06-09 13:26:32 +0530

    Fix database connection timeout crash by running database queries on Dispatchers.IO and offloading PDF parsing to Dispatchers.Default

--- shared/src/commonMain/kotlin/com/ssbmax/pdfparser/di/Koin.kt
+++ shared/src/commonMain/kotlin/com/ssbmax/pdfparser/di/Koin.kt
@@ -5,6 +5,8 @@ import com.ssbmax.pdfparser.database.PayslipDatabase
 import com.ssbmax.pdfparser.database.getDatabaseBuilder
 import com.ssbmax.pdfparser.parser.PdfParser
 import com.ssbmax.pdfparser.repository.PayslipRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -13,6 +15,7 @@ val sharedModule: Module = module {
     single<PayslipDatabase> {
         getDatabaseBuilder()
             .setDriver(BundledSQLiteDriver())
+            .setQueryCoroutineContext(Dispatchers.IO)
             .build()
     }
     
