8086859c65044b5d1b1c894e56f667ba5c4084ad
sunilpawar <mail.sunilpawar@gmail.com>
2026-06-09 16:14:36 +0530

    Fix Indian comma formatting and improve coordinate-aware parsing

--- composeApp/src/commonMain/kotlin/com/ssbmax/pdfparser/ui/screens/DashboardScreen.kt
+++ composeApp/src/commonMain/kotlin/com/ssbmax/pdfparser/ui/screens/DashboardScreen.kt
@@ -244,12 +244,25 @@ private fun AllocationLegend(values: List<Float>, gross: Double, colors: List<Co
 
 private fun formatAmount(amount: Double): String {
     val longVal = amount.toLong()
-    return if (longVal >= 100000) {
+    if (longVal >= 100000) {
         val lakhs = longVal / 100000.0
-        "${lakhs.toString().take(4)}L"
-    } else {
-        longVal.toString().replace(Regex("(\\d)(?=(\\d{2})+(?!\\d))")) { match ->
-            match.groupValues[1] + ","
+        return "${lakhs.toString().take(4)}L"
+    }
+    val str = longVal.toString()
+    if (str.length <= 3) return str
+    val lastThree = str.substring(str.length - 3)
+    val remaining = str.substring(0, str.length - 3)
+    val builder = StringBuilder()
+    var i = remaining.length
+    while (i > 0) {
+        if (i >= 2) {
+            builder.insert(0, remaining.substring(i - 2, i))
+            if (i - 2 > 0) builder.insert(0, ",")
+            i -= 2
+        } else {
+            builder.insert(0, remaining.substring(0, 1))
+            i -= 1
         }
     }
+    return "${builder.toString()},$lastThree"
 }
