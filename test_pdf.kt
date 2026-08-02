229bc1d3035311d976db558e46b22d465e8103a9
sunilpawar <mail.sunilpawar@gmail.com>
2026-08-02 11:55:18 +0530

    refactor: remove root scratch and legacy prototype files

--- test_pdf.kt
+++
@@ -1,4 +0,0 @@
-import platform.PDFKit.PDFPage
-fun test(page: PDFPage) {
-    page.characterBoundsAtIndex(0)
-}
--- test_pdf2.kt
+++
@@ -1,6 +0,0 @@
-import platform.PDFKit.PDFPage
-import kotlinx.cinterop.ExperimentalForeignApi
-@OptIn(ExperimentalForeignApi::class)
-fun test(page: PDFPage) {
-    val rect = page.characterBoundsAtIndex(0)
-}
